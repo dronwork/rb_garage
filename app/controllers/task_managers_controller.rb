4c777b012517cf2a5de74427b69d813383799310
Andrei Keida <dronwork@outlook.com>
2018-02-19 13:18:56 +0200

    Add gem devise

--- app/controllers/task_managers_controller.rb
+++ app/controllers/task_managers_controller.rb
@@ -1,4 +1,5 @@
 class TaskManagersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_task_manager, only: [:show, :edit, :update, :destroy]
 
   # GET /task_managers
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :task_managers
   root 'task_managers#index'
 end
